c7951f91e2f03aa1d3af3ddbb363170761d78427
fmohamedfaras <fmohamedfaras@gmail.com>
2026-04-13 10:06:28 +0800

    Add missing import line

--- src/test/java/seedu/inventorybro/command/AddCommandTest.java
+++ src/test/java/seedu/inventorybro/command/AddCommandTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.inventorybro.Category;
 import seedu.inventorybro.CategoryList;
 import seedu.inventorybro.Item;
 import seedu.inventorybro.ItemList;
